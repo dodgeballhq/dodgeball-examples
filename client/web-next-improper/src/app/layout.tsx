16e22f059ab2770a6289959697e5e3190a774560
Ashkan-Dodgeball <109244393+Ashkan-Dodgeball@users.noreply.github.com>
2024-11-13 16:19:05 -0800

    initial web-next-improper setup

---
+++ client/web-next-improper/src/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Client/Next.js | Dodgeball Examples",
+  description: "This example demonstrates how to use the Dodgeball Client SDK with Next.js.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        suppressHydrationWarning={true}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
